c00d26fbd514a86ce6457305dab8a3dde0f896f1
Cameron Maccoll <cammaccoll@gmail.com>
2018-01-27 15:21:28 -0500

    Finished preliminary setup and commenting

--- src/main/java/ca/_4976/powerup/subsystems/Elevator.java
+++ src/main/java/ca/_4976/powerup/subsystems/Elevator.java
@@ -1,6 +1,9 @@
 package ca._4976.powerup.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
+/*
+Made by Cameron, Jacob, Ethan, Zach
+*/
+
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
@@ -9,58 +12,78 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 
 public final class Elevator extends Subsystem implements Runnable, Sendable{
 
-    private final TalonSRX liftMotor = new TalonSRX(2);
-    private final TalonSRX liftSlave = new TalonSRX(3);
+    //Main motor for moving elevator up and down
+    private final TalonSRX elevMotor = new TalonSRX(2);
 
-    private final DigitalInput limitSwitchMax = new DigitalInput(4); //top switch normally held closed
-    private final DigitalInput limitSwitchMin = new DigitalInput(5); //bottom switch normally held open
+    //Elevator slaved to main motor
+    private final TalonSRX elevSlave = new TalonSRX(3);
 
-    //private final Encoder liftEnc = new Encoder(); work on 1-port enc
+    //Limit switch near top of the first stage of the elevator. Switch normally held closed
+    private final DigitalInput limitSwitchMax = new DigitalInput(4);
 
-    public boolean end = false;
+    //Limit switch near bottom of the first stage of the elevator. Switch normally held open
+    private final DigitalInput limitSwitchMin = new DigitalInput(5);
+
+    //Encoder on the elevator motors
+    private final Encoder elevEnc = new Encoder(4,5);
+
+    //Height variable for measurements
+    public double elevHeight = 0;
+
 
     @Override
     protected void initDefaultCommand() {
 
     }
 
+
     @Override
     public void run() {
-
+        //Use PID to move elevator to setpoint
     }
 
+    //Reads encoders to return current height of the elevator with reference to it's zero point
     public double getHeight(){
         return 0;
     }
 
-    public void setHeight(int height){
+    //Uses PID control to set a height using encoder values -> moves to set height
+    public void setHeight(double height){
 
     }
 
+    //Increases setpoint height
     public void moveUp(){
 
     }
 
+    //Decreases setpoint height
     public void moveDown(){
 
     }
 
+    //Sets setpoint to ground level preset
     public void groundPS(){
 
     }
 
+    //Sets setpoint to switch level preset
     public void switchPS(){
 
     }
 
+    //Sets setpoint to the low scale level preset
     public void scaleLowPS(){
 
     }
 
+    //Sets setpoint to the mid scale level preset
     public void scaleMidPS(){
 
     }
 
+    //Sets setpoint to the highest scale level preset
+    //May or not be max elevator height, depending on whether linkarm is necessary to reach top of scale at full height
     public void scaleHighPS(){
 
     }
--- src/main/java/ca/_4976/powerup/subsystems/LinkArm.java
+++ src/main/java/ca/_4976/powerup/subsystems/LinkArm.java
@@ -1,12 +1,22 @@
 package ca._4976.powerup.subsystems;
 
+/*
+Made by Cameron, Jacob, Ethan, Zach
+*/
+
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.Sendable;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public final class LinkArm extends Subsystem implements Runnable, Sendable {
 
+    //Motor inside linkarm carriage - raises/lowers arm
     private final TalonSRX armMotor = new TalonSRX(4);
+
+    //Angle variable for measurements
+    private double armAngle = 0;
+
+    //Add LinkArm encoder built into the TalonSRX -> research code
 
     @Override
     protected void initDefaultCommand() {
@@ -18,18 +28,29 @@ public final class LinkArm extends Subsystem implements Runnable, Sendable {
 
     }
 
+    //ARM WILL USE 0 DEGREES AS REFERENCE
+    //Equivalent arm parallel with ground
+    //All angle setting/holding will be done using PID
+
+    //Read joystick input to increase armAngle -> raises LinkArm - waiting on max angle
+    //Will prevent movement past this point
     public void raiseAngle(){
 
     }
 
+    //Read joystick input to decrease armAngle -> lowers LinkArm - waiting on max angle
+    //Will prevent movement past this point
     public void lowerAngle(){
 
     }
 
-    public void setAngle(int angle){
+    //Pass an angle as a double to move the arm to that angle
+    //Use PID to set angle using motors
+    public void setAngle(double angle){
 
     }
 
+    //Read the arm's encoder to get the current angle of the arm with reference to middle
     public double getAngle(){
         return 0;
     }

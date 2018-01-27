6bcfa3877a064617e9996a7e7e5c6b532eac645e
Michael Mann <mjam200@gmail.com>
2018-01-27 14:33:20 -0500

    a fresh start

---
+++ src/main/java/ca/_4976/powerup/subsystems/GrabMan.java
@@ -0,0 +1,11 @@
+package ca._4976.powerup.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class GrabMan extends Subsystem {
+    
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+}

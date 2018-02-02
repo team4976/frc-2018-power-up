9265952a319830000dc0d770294815614a726351
Cameron Maccoll <cammaccoll@gmail.com>
2018-02-01 22:43:09 -0500

    Added preset encoder constants and prelim EPS code

--- src/main/java/ca/_4976/powerup/subsystems/Elevator.java
+++ src/main/java/ca/_4976/powerup/subsystems/Elevator.java
@@ -6,15 +6,9 @@ Made by Cameron, Jacob, Ethan, Zach
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.sun.corba.se.impl.encoding.EncapsInputStream;
 import edu.wpi.first.wpilibj.*;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.hal.EncoderJNI;
-import sun.nio.cs.ext.DoubleByte;
-import sun.security.krb5.internal.EncKDCRepPart;
-
-import java.security.spec.EncodedKeySpec;
 
 
 public final class Elevator extends Subsystem implements Runnable, Sendable {
@@ -22,31 +16,36 @@ public final class Elevator extends Subsystem implements Runnable, Sendable {
     //Main motor for moving elevator up and down
     private final TalonSRX elevMotorMain = new TalonSRX(2);
 
-    //Elevator slave motor 1
-    private final TalonSRX elevSlave1 = new TalonSRX(3);
+    //Elevator slave motors
+    private final TalonSRX elevSlave1 = new TalonSRX(3),
+    elevSlave2 = new TalonSRX(8);
 
-    //Elevator slave motor 2
-    private final TalonSRX elevSlave2 = new TalonSRX(8);
-
-    //Operator Elevator Left JoyStick
-    private final Joystick leftOperatorJoy = new Joystick(0);
-
+    //Operator left Stick - elevator control
+    private final Joystick leftOpJoy = new Joystick(0);
 
     //Driver Elevator Right JoyStick
 
-
-    //Limit switch near top of the first stage of the elevator. Switch normally held closed
+    //Limit switch near top of the first stage of the elevator. Switch normally held open
     private final DigitalInput limitSwitchMax = new DigitalInput(4);
 
-    //Limit switch near bottom of the first stage of the elevator. Switch normally held open
+    //Limit switch near bottom of the first stage of the elevator. Switch normally held closed
     private final DigitalInput limitSwitchMin = new DigitalInput(5);
 
     //Encoder on the elevator motors
     private final Encoder elevEnc = new Encoder(4, 5);
 
 
-    //Height variable for measurements
-    public double elevHeight = 0;
+    //Presets - encoder values from excel sheet (Elevator Distance Chart.xlsx)
+    private final double EPS_SCALE_HIGH = 24275.63,
+    EPS_SCALE_MID = 19073.71,
+    EPS_SCALE_LOW = 13871.79,
+    EPS_SWITCH = 1733.97,
+    EPS_GROUND = 0,
+    ELEVATOR_MAX = EPS_SCALE_HIGH,
+    ELEVATOR_MIN = EPS_GROUND,
+
+    //ABOUT 2 CM OF TOLERANCE IN VALUES - MAY BE UPDATED
+    TOL_RANGE = 340;
 
 
     @Override
@@ -57,69 +56,103 @@ public final class Elevator extends Subsystem implements Runnable, Sendable {
 
 
     public void run() {
-        //Use PID to move elevator to setpoint
+        //Maybe put moveElevator method in here?
     }
 
     //Reads encoders to return current height of the elevator with reference to it's zero point
     public double getHeight() {
-        return 0;
+        return elevEnc.get();
     }
 
 
     public void moveElevator() {
-        //THIS WILL RESULT IN VERY JERKY MOTION
-        double elevOut = 0;
 
-        //Dead zone (stopped)
-        if (Math.abs(leftOperatorJoy.getY()) < 0.05) {
+        double input = leftOpJoy.getY();
+
+        //JOYSTICK DEAD ZONE
+        if(Math.abs(input) < 0.03){
             elevMotorMain.set(ControlMode.PercentOutput, 0);
         }
 
-
-        //UP
-        //Less than or 50% pressed
-        else if(Math.abs(leftOperatorJoy.getY()) <= 0.5) { //ABSOLUTE VALUE BEING CHECKED FOR >= AGAINST A NEGATIVE?
-            elevOut = (leftOperatorJoy.getY() * 0.4);
+        //CHECK FOR MAX / MIN
+        else if(getHeight() < ELEVATOR_MAX - TOL_RANGE || getHeight() > ELEVATOR_MIN + TOL_RANGE){
+            elevMotorMain.set(ControlMode.PercentOutput, input * 0.7 * 100);
         }
-
-        //Joystick > 50%
-        else if((leftOperatorJoy.getY()) > 0.5) {
-            elevOut = (leftOperatorJoy.getY() * 0.8) - 0.2;
-        }
-
-        //DOWN
-
-        //Less than 50% pressed
-        else if ((leftOperatorJoy.getY()) < -0.5) {
-            elevOut = (leftOperatorJoy.getY() * 0.8) + 0.2;
-        }
-
-        elevMotorMain.set(ControlMode.PercentOutput, elevOut);
     }
 
 
     //Moves elevator until height is within certain range of set value - at ground level
     public void groundPS() {
-        double groundPS = elevEnc
+
+        while(getHeight() < EPS_GROUND - TOL_RANGE || getHeight() > EPS_GROUND + TOL_RANGE){
+
+            if(getHeight() < EPS_GROUND - TOL_RANGE){
+                elevMotorMain.set(ControlMode.PercentOutput, 0.5);
+            }
+
+            else if(getHeight() > EPS_GROUND + TOL_RANGE){
+                elevMotorMain.set(ControlMode.PercentOutput, -0.5);
+            }
+        }
     }
 
     //Moves elevator until height is within certain range of set value - at switch level
     public void switchPS() {
 
+        while(getHeight() < EPS_SWITCH - TOL_RANGE || getHeight() > EPS_SWITCH + TOL_RANGE){
+
+            if(getHeight() < EPS_SWITCH - TOL_RANGE){
+                elevMotorMain.set(ControlMode.PercentOutput, 0.5);
+            }
+
+            else if(getHeight() > EPS_SWITCH + TOL_RANGE){
+                elevMotorMain.set(ControlMode.PercentOutput, -0.5);
+            }
+        }
     }
 
     //Moves elevator until height is within certain range of set value - at low scale level
     public void scaleLowPS() {
+        
+        while(getHeight() < EPS_SCALE_LOW - TOL_RANGE || getHeight() > EPS_SCALE_LOW + TOL_RANGE){
 
+            if(getHeight() < EPS_SCALE_LOW - TOL_RANGE){
+                elevMotorMain.set(ControlMode.PercentOutput, 0.5);
+            }
+
+            else if(getHeight() > EPS_SCALE_LOW + TOL_RANGE){
+                elevMotorMain.set(ControlMode.PercentOutput, -0.5);
+            }
+        }
     }
 
     //Moves elevator until height is within certain range of set value - at mid scale level
     public void scaleMidPS() {
 
+        while(getHeight() < EPS_SCALE_MID - TOL_RANGE || getHeight() > EPS_SCALE_MID + TOL_RANGE){
+
+            if(getHeight() < EPS_SCALE_MID - TOL_RANGE){
+                elevMotorMain.set(ControlMode.PercentOutput, 0.5);
+            }
+
+            else if(getHeight() > EPS_SCALE_MID + TOL_RANGE){
+                elevMotorMain.set(ControlMode.PercentOutput, -0.5);
+            }
+        }
     }
 
     //Moves elevator until height is within certain range of set value - at high scale level
     public void scaleHighPS() {
 
+        while(getHeight() < EPS_SCALE_HIGH - TOL_RANGE || getHeight() > EPS_SCALE_HIGH + TOL_RANGE){
+
+            if(getHeight() < EPS_SCALE_HIGH - TOL_RANGE){
+                elevMotorMain.set(ControlMode.PercentOutput, 0.5);
+            }
+
+            else if(getHeight() > EPS_SCALE_HIGH + TOL_RANGE){
+                elevMotorMain.set(ControlMode.PercentOutput, -0.5);
+            }
+        }
     }
 }

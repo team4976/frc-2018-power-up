229a0396be025fc662631ac8f62094525a8cd2b9
Aavin sidhu <aavinsidhu@hotmail.com>
2018-02-03 09:24:05 -0500

    merged with motion

--- src/main/java/ca/_4976/powerup/OI.java
+++ src/main/java/ca/_4976/powerup/OI.java
@@ -1,5 +1,8 @@
 package ca._4976.powerup;
 
+import ca._4976.powerup.commands.RecordProfile;
+import ca._4976.powerup.commands.RunProfile;
+import ca._4976.powerup.commands.Transmission;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
@@ -8,12 +11,16 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
  * robot to allow control with a single PS3 joystick. As a result, not all
  * functionality from the real robot is available.
  */
+
 public final class OI {
 
     public Joystick driver = new Joystick(0);
     public Joystick operator = new Joystick(1);
 
-    public OI() {
+    OI() {
 
+        new JoystickButton(driver, 7).whenPressed(new RecordProfile());
+        new JoystickButton(driver, 8).whenPressed(new RunProfile());
+        new JoystickButton(driver, 4).whenPressed(new Transmission());
     }
 }
--- src/main/java/ca/_4976/powerup/Robot.java
+++ src/main/java/ca/_4976/powerup/Robot.java
@@ -1,12 +1,18 @@
 package ca._4976.powerup;
 
-import ca._4976.powerup.OI;
+
+
+import ca._4976.powerup.data.Profile;
+import ca._4976.powerup.subsystems.Drive;
+import ca._4976.powerup.subsystems.Motion;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import static ca.qormix.library.Lazy.use;
 
 /**
  * This is the main class for running the Robot code.
@@ -17,26 +23,61 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
+
 public final class Robot extends IterativeRobot {
 
     public static OI oi;
 
-    @Override public void robotInit() {
+    public final static Drive drive = new Drive();
+    public final static Motion motion = new Motion();
 
+    private final NetworkTableInstance instance = NetworkTableInstance.getDefault();
+    private final NetworkTable table = instance.getTable("Log");
+
+    private final NetworkTableEntry leftDistance =  table.getEntry("Left Distance");
+    private final NetworkTableEntry rightDistance =  table.getEntry("Right Distance");
+    private final NetworkTableEntry stopped =  table.getEntry("Is Stopped");
+
+    private final NetworkTableEntry profiles =  table.getEntry("Profiles");
+
+    @Override public void robotInit() {
         oi = new OI();
 
+        SmartDashboard.putData(drive);
+        SmartDashboard.putData(motion);
+
+        Robot.drive.defaultGear();
     }
+
+
 
     @Override public void disabledInit() {
-
+        motion.stop();
     }
+
+
 
     @Override public void autonomousPeriodic(){
+        Scheduler.getInstance().run();
+        log();
     }
+
+
 
     @Override public void teleopPeriodic(){
+        Scheduler.getInstance().run();
+        log();
     }
 
+
+
     private void log() {
+        use(drive.getEncoderPosition(), it -> {
+            leftDistance.setNumber(it[0]);
+            rightDistance.setNumber(it[1]);
+        });
+
+        stopped.setBoolean(drive.isStopped());
+        profiles.setStringArray(Profile.getAvailableProfiles());
     }
 }

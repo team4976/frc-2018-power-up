9164a2d68b26f5fc5ec5ce697e2ca4065029a826
Aavin sidhu <aavinsidhu@hotmail.com>
2018-02-16 21:18:11 -0500

    first working code

--- src/main/java/ca/_4976/powerup/subsystems/CubeHandler.java
+++ src/main/java/ca/_4976/powerup/subsystems/CubeHandler.java
@@ -14,7 +14,6 @@ import static ca.qormix.library.Lazy.use;
 //Main superclass that holds all the methods used by the commands
 public final class CubeHandler extends Subsystem implements Sendable {
     public final TalonSRX grabberI = new TalonSRX(0);
-    private final TalonSRX grabberII = new TalonSRX(1);
 
     private double speedFast=0, notFast=0, grabCurrent=0;
     public CubeHandler(){
@@ -39,25 +38,20 @@ public final class CubeHandler extends Subsystem implements Sendable {
     public void grab() {//grabs cube
         System.out.println("my boi is to grab me");
         grabberI.set(ControlMode.PercentOutput, 0.8);
-        grabberII.set(ControlMode.PercentOutput, -0.8);
         System.out.println("Grabber 1 sped "+grabberI.getMotorOutputPercent());
-        System.out.println("Grabber 2 sped "+grabberII.getMotorOutputPercent());
     }
     public void stop(){//Stops the grabber motors
         System.out.println("no longer moving");
         grabberI.set(ControlMode.PercentOutput, 0);
-        grabberII.set(ControlMode.PercentOutput, 0);
     }
     public void release() {//Releases cube from bot
         System.out.println("ejection is in effect");
         grabberI.set(ControlMode.PercentOutput, -0.1);
-        grabberII.set(ControlMode.PercentOutput, 0.1);
     }
 
     public void slow(){//spins motors slow when we have a cube
         System.out.println("have cube");
         grabberI.set(ControlMode.PercentOutput, 0.05);
-        grabberII.set(ControlMode.PercentOutput, -0.05);
     }
 
     public boolean checkCurrent(){
